1e4437adc3ee580d725cefac1b029653e9dc7a11
void2610 <izumisan2610@gmail.com>
2024-08-15 03:04:48 +0900

    ボス修正

---
+++ Assets/Scripts/Util/FloatMove.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class FloatMove : MonoBehaviour
+{
+    [SerializeField]
+    private float moveDistance = 0.2f;
+    [SerializeField]
+    private float moveDuration = 1f;
+    [SerializeField]
+    private float delay = 0f;
+
+    private void StartMove()
+    {
+        transform.DOLocalMoveY(moveDistance, moveDuration).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+    }
+    private void Awake()
+    {
+        Invoke("StartMove", delay);
+    }
+}
